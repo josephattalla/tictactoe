e6f13bbfca2bb058937938e75012677e0a060618
Joseph Attalla <jojoatt10@gmail.com>
2024-01-04 19:53:13 +0000

    Jan 4, 2024, 2:53 PM

--- functions.cpp
+++ functions.cpp
@@ -39,7 +39,8 @@ char players(const board[3][3])
     {
         for (int j = 0; j < 3; j++) 
         {
-            if (board[i][j] != BOARD[i][j]) {
+            if (board[i][j] != BOARD[i][j]) 
+            {
                 // The boards are different
                 // Do something here, like return an indicator or perform some other action
             }
