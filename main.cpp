8cba0348655c6f1f1affacbeeed0744dd947bfaf
Joseph Attalla <jojoatt10@gmail.com>
2024-01-04 20:04:27 +0000

    Jan 4, 2024, 3:04 PM

--- main.cpp
+++ main.cpp
@@ -3,17 +3,6 @@
 #include "header.hpp"
 using namespace std;
 
-// Array for the board
-const char BOARD[3][3] = {
-  {'1', '2', '3'}, 
-  {'4', '5', '6'}, 
-  {'7', '8', '9'}
-};
-
-// Variables for the players
-const char X = 'X';
-const char O = 'O';
-
 
 int main() 
 {
