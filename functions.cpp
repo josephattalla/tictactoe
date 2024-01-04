950185635a1efc4a4e8d0f619417d0b54bd36b30
Joseph Attalla <jojoatt10@gmail.com>
2024-01-04 20:34:15 +0000

    Jan 4, 2024, 3:34 PM

--- functions.cpp
+++ functions.cpp
@@ -1,113 +1,195 @@
-#include "header.hpp"
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
 
-
-void print_board(const char board[3][3])
-{
-    /*
-    Prints the Tic-Tac-Toe board
-    */
-
-    for (int i = 0; i < 3; i++)
-    {
-        for (int j = 0; j < 3; j++)
-        {
-            cout << "|" << setw(2) << board[i][j] << setw(3);
-        }
-        if (i < 2)
-        {
-            cout << "\n_______________\n";
-        }
-    }
-    cout << endl;
+//draw board
+void db(char board[]) {
+  cout << board[0] << "|" << board[1] << "|" << board[2] << endl;
+  cout << "-----" << endl;
+  cout << board[3] << "|" << board[4] << "|" << board[5] << endl;
+  cout << "-----" << endl;
+  cout << board[6] << "|" << board[7] << "|" << board[8] << endl;
 }
 
-char player(const char board[3][3])
-{
-    /*
-    Returns whose turn it is.
-    If the game just started, X goes first.
-    */
 
-    // Variables to count X's and O's
-    int X_count = 0;
-    int O_count = 0;
-
-    // Loop through rows
-    for (int i = 0; i < 3; i++)
-    {
-        // Loop through columns
-        for (int j = 0; j < 3; j++)
-        {
-            if (board[i][j] == X)
-            {
-                X_count++;
-            }
-            else if (board[i][j] == O)
-            {
-                O_count++;
-            }
-        }
-    }
-
-    // If less or equal X, return X, else return O
-    return (X_count <= O_count) ? X : O;
+//player 1 turn
+void p1turn(int p1, int p2, char board[]) {
+while (p1 < 1 || p1 > 9 || board[p1 - 1] == 'o' || board[p1 - 1] == 'x') {
+cout << "Player 1 enter where you would like to go: ";
+cin >> p1;
 }
 
-bool isValidMove(const char board[3][3], const pair<int, int> &action)
-{
-    /*
-    Checks if the move is valid.
-    */
-
-    return (action.first >= 0 && action.first < BOARD_SIZE &&
-            action.second >= 0 && action.second < BOARD_SIZE &&
-            board[action.first][action.second] != X && board[action.first][action.second] != O);
+ switch (p1) {
+   case 1:
+  if (p1 != p2) {
+    board[0] = 'x';
+    p1 = 1;
+  }
+    break;
+    case 2:
+      if (p1 != p2) {
+    board[1] = 'x';
+    p1 = 2;
+  }
+  
+    break;
+    case 3:
+      if (p1 != p2) {
+    board[2] = 'x';
+    p1 = 3;
+  }
+    break;
+    case 4:
+      if (p1 != p2) {
+    board[3] = 'x';
+    p1 = 4;
+  }
+    break;
+    case 5:
+      if (p1 != p2) {
+    board[4] = 'x';
+    p1 = 5;
+  }
+    break;
+    case 6:
+      if (p1 != p2) {
+    board[5] = 'x';
+    p1 = 6;
+  }
+    break;
+    case 7:
+      if (p1 != p2) {
+    board[6] = 'x';
+    p1 = 7;
+  }
+    break;
+    case 8:
+      if (p1 != p2) {
+    board[7] = 'x';
+    p1 = 8;
+  }
+    break;
+    case 9:
+      if (p1 != p2) {
+    board[8] = 'x';
+    p1 = 9;
+  }
+    break;
+    default:
+    std::cout << "Invalid\n";
+    break;
+ }
 }
 
-set<pair<int, int> > actions(const char board[3][3])
-{
-    /*
-    Returns all possible moves.
-    */
-
-    set<pair<int, int> > moves;
-
-    for (int i = 0; i < BOARD_SIZE; ++i)
-    {
-        for (int j = 0; j < BOARD_SIZE; ++j)
-        {
-            // Check if the cell is empty
-            if (board[i][j] != X && board[i][j] != O)
-            {
-                moves.insert(make_pair(i, j));
-            }
-        }
-    }
-
-    return moves;
+//p2 turn
+void p2turn(int p1, int p2, char board[]) {
+while (p2 < 1 || p2 > 9 || board[p2 - 1] == 'x' || board[p2 - 1] == 'o') {
+cout << "Player 2 enter where you would like to go: ";
+cin >> p2;
 }
 
-array<array<char, 3>, 3> result(const array<array<char, 3>, 3> &board, const pair<int, int> &action)
+ switch (p2) {
+   case 1:
+  if (p1 != p2) {
+    board[0] = 'o';
+  }
+    break;
+    case 2:
+      if (p1 != p2) {
+    board[1] = 'o';
+  }
+    break;
+    case 3:
+      if (p1 != p2) {
+    board[2] = 'o';
+  }
+    break;
+    case 4:
+      if (p1 != p2) {
+    board[3] = 'o';
+  }
+    break;
+    case 5:
+      if (p1 != p2) {
+    board[4] = 'o';
+  }
+    break;
+    case 6:
+      if (p1 != p2) {
+    board[5] = 'o';
+  }
+    break;
+    case 7:
+      if (p1 != p2) {
+    board[6] = 'o';
+  }
+    break;
+    case 8:
+      if (p1 != p2) {
+    board[7] = 'o';
+  }
+    break;
+    case 9:
+      if (p1 != p2) {
+    board[8] = 'o';
+  }
+    break;
+    default:
+    std::cout << "Invalid\n";
+    break;
+ }
+}
+
+bool gameover(char board[])
 {
-    /*
-    Returns the board that results from making a move (i, j) on the board.
-    */
-
-    // Check if the move is valid
-    if (!isValidMove(board.data(), action))
-    {
-        throw runtime_error("Invalid Move!");
-    }
-
-    // Make a deep copy of the board
-    array<array<char, 3>, 3> newBoard = board;
-
-    // Get whose turn it is
-    char user = player(board.data());
-
-    // Place user (player) in action (empty index on board)
-    newBoard[action.first][action.second] = user;
-
-    // Return the new board
-    return newBoard;
+      if (board[0] == 'x' && board[1] == 'x' && board[2] == 'x') {
+    cout << "gameover Player 1 wins!\n";
+    return true;
+  } else if (board[3] == 'x' && board[4] == 'x' && board[5] == 'x') {
+    cout << "gameover Player 1 wins!\n";
+    return true;
+  } else if (board[6] == 'x' && board[7] == 'x' && board[8] == 'x') {
+    cout << "gameover Player 1 wins!\n";
+    return true; 
+  } else if (board[0] == 'x' && board[3] == 'x' && board[6] == 'x') {
+    cout << "gameover Player 1 wins!\n";
+    return true;
+  } else if (board[1] == 'x' && board[4] == 'x' && board[7] == 'x') {
+    cout << "gameover Player 1 wins!\n";
+    return true;
+  } else if (board[2] == 'x' && board[5] == 'x' && board[8] == 'x') {
+    cout << "gameover Player 1 wins!\n";
+    return true;
+  } else if (board[0] == 'x' && board[4] == 'x' && board[8] == 'x') {
+    cout << "gameover Player 1 wins!\n";
+    return true;
+  } else if (board[2] == 'x' && board[4] == 'x' && board[6] == 'x') {
+    cout << "gameover Player 1 wins!\n";
+    return true;
+  } else if (board[0] == 'o' && board[1] == 'o' && board[2] == 'o') {
+    cout << "gameover Player 2 wins!\n";
+    return true;
+  } else if (board[3] == 'o' && board[4] == 'o' && board[5] == 'o') {
+    cout << "gameover Player 2 wins!\n";
+    return true;
+  } else if (board[6] == 'o' && board[7] == 'o' && board[8] == 'o') {
+    cout << "gameover Player 2 wins!\n";
+    return true; 
+  } else if (board[0] == 'o' && board[3] == 'o' && board[6] == 'o') {
+    cout << "gameover Player 2 wins!\n";
+    return true;
+  } else if (board[1] == 'o' && board[4] == 'o' && board[7] == 'o') {
+    cout << "gameover Player 2 wins!\n";
+    return true;
+  } else if (board[2] == 'o' && board[5] == 'o' && board[8] == 'o') {
+    cout << "gameover Player 2 wins!\n";
+    return true;
+  } else if (board[0] == 'o' && board[4] == 'o' && board[8] == 'o') {
+    cout << "gameover Player 2 wins!\n";
+    return true;
+  } else if (board[2] == 'o' && board[4] == 'o' && board[6] == 'o') {
+    cout << "gameover Player 2 wins!\n";
+    return true;
+  }
+  return false;
 }
