b9885d14d35b40d2071e5c5ddaedfc9c05aa31f5
Joseph Attalla <jojoatt10@gmail.com>
2024-01-04 19:40:59 +0000

    Jan 4, 2024, 2:40 PM

--- header.hpp
+++ header.hpp
@@ -1,4 +1,4 @@
-void print_board(const char board[]);
+void print_board(const char board[3]);
 void p1turn(int p1, int p2, char board[]);
 void p2turn(int p1, int p2, char board[]);
 bool gameover(char board[]);
