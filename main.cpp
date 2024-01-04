1adfc5afefba08c009ec925eb17234fa0141c025
Joseph Attalla <jojoatt10@gmail.com>
2024-01-04 19:36:46 +0000

    Jan 4, 2024, 2:36 PM

--- main.cpp
+++ main.cpp
@@ -5,7 +5,9 @@ using namespace std;
 
 // Array for the board
 const char BOARD[] = {
-  {"1", "2", "3"}, {"4", "5", "6"}, {"7", "8", "9"}};
+  {"1", "2", "3"}, 
+  {"4", "5", "6"}, 
+  {"7", "8", "9"}};
 
 // Variables for the players
 const char X = "X";
