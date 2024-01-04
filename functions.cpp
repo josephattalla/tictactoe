82da7044369750f5a9555f7062891f1692838d9a
Joseph Attalla <jojoatt10@gmail.com>
2024-01-04 19:41:53 +0000

    Jan 4, 2024, 2:41 PM

--- functions.cpp
+++ functions.cpp
@@ -14,7 +14,6 @@ void print_board(const char board[3][3])
   for (int i = 0; i < 3; i++)
   {
     cout << "|" << setw(5) << "|" << setw(5) << "|" << endl;
-    cout << "|";
     // loop through columns
     for (int j = 0; j < 3; j++)
     {
