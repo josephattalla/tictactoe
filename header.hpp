7fd894fb9a56ee86594981327bd7a97a0b901472
Joseph Attalla <jojoatt10@gmail.com>
2024-01-04 20:04:33 +0000

    Jan 4, 2024, 3:04 PM

--- header.hpp
+++ header.hpp
@@ -1,3 +1,14 @@
 void print_board(const char board[3][3]);
 char player(const board[3][3]);
 bool gameover(char board[]);
+
+// Array for the board
+const char BOARD[3][3] = {
+  {'1', '2', '3'}, 
+  {'4', '5', '6'}, 
+  {'7', '8', '9'}
+};
+
+// Variables for the players
+const char X = 'X';
+const char O = 'O';
