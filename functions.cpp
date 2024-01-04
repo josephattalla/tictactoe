0126b76679be15f98909cf5b51a4bacb73557fa7
Joseph Attalla <jojoatt10@gmail.com>
2024-01-04 19:55:11 +0000

    Jan 4, 2024, 2:55 PM

--- functions.cpp
+++ functions.cpp
@@ -37,6 +37,7 @@ char players(const board[3][3])
   // if board == BOARD, return X
   // count how many X and O on board to determine whose turn
   bool equal;
+  
   for (int i = 0; i < 3; i++) 
   {
       for (int j = 0; j < 3; j++) 
