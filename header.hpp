bb0b486defd300e6518cbf659dc3b5c1f087a489
Joseph Attalla <jojoatt10@gmail.com>
2024-01-04 20:33:14 +0000

    Jan 4, 2024, 3:33 PM

--- header.hpp
+++ header.hpp
@@ -1,19 +1,4 @@
-#include <iostream>
-#include <iomanip>
-#include <set>
-#include <stdexcept>
-#include <array>
-
-const char X = 'X';
-const char O = 'O';
-const char EMPTY = ' ';
-
-const int BOARD_SIZE = 3;
-
-using namespace std;
-
-void print_board(const array<array<char, BOARD_SIZE>, BOARD_SIZE>& board);
-char player(const array<array<char, BOARD_SIZE>, BOARD_SIZE>& board);
-bool isValidMove(const array<array<char, BOARD_SIZE>, BOARD_SIZE>& board, const pair<int, int>& action);
-set<pair<int, int>> actions(const array<array<char, BOARD_SIZE>, BOARD_SIZE>& board);
-array<array<char, BOARD_SIZE>, BOARD_SIZE> result(const array<array<char, BOARD_SIZE>, BOARD_SIZE>& board, const pair<int, int>& action);
+void db(char board[]);
+void p1turn(int p1, int p2, char board[]);
+void p2turn(int p1, int p2, char board[]);
+bool gameover(char board[]);
