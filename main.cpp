a815722cd637b1ca3bfc5ee3244a03d51d24198f
Joseph Attalla <jojoatt10@gmail.com>
2024-01-04 19:23:49 +0000

    Jan 4, 2024, 2:23 PM

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 //Array for the board
-char const BOARD[3][3] = {{"1", "2", "3"},
+const char BOARD[3][3] = {{"1", "2", "3"},
                     {"4", "5", "6"},
                     {"7", "8", "9"}};
 
