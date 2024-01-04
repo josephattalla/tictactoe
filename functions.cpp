a176544d8e44405fe7cc5d14ab954057cc8b6985
Joseph Attalla <jojoatt10@gmail.com>
2024-01-04 19:21:01 +0000

    Jan 4, 2024, 2:21 PM

--- functions.cpp
+++ functions.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 //draw board
-void db(char board[]) {
+void print_board(char board[]) {
   cout << board[0] << "|" << board[1] << "|" << board[2] << endl;
   cout << "-----" << endl;
   cout << board[3] << "|" << board[4] << "|" << board[5] << endl;
