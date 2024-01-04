777c432755117d4d3662ecb222945a55c7c77bf7
Joseph Attalla <jojoatt10@gmail.com>
2024-01-04 20:21:16 +0000

    Jan 4, 2024, 3:21 PM

--- functions.cpp
+++ functions.cpp
@@ -95,7 +95,7 @@ set<pair<int, int>> actions(const char board[3][3])
         for (int j = 0; j < BOARD_SIZE; ++j) 
         {
             // Check if the cell is empty (assuming ' ' represents an empty cell)
-            if (board[i][j] != X)
+            if (board[i][j] != X and )
             {
                 moves.insert(make_pair(i, j));
             }
