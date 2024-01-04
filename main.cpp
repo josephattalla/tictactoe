8634a68b078fbb453a1ecf7112a441f7c349b251
Joseph Attalla <jojoatt10@gmail.com>
2024-01-04 19:33:40 +0000

    Jan 4, 2024, 2:33 PM

--- main.cpp
+++ main.cpp
@@ -15,49 +15,5 @@ const char O = "O"
 
 int main() 
 {
-  //drawboard
-  print_board(board);
-
-
-  while (turns < 6) 
-  {
-    //p1 turn
-    p1turn(p1, p2, board);
-
-    //draw board
-    print_board(board);
-
-    //add to turns
-    turns++;
-
-    //check if game is over
-    if (gameover(board) == true)
-    {
-      return 0;
-    }
-
-    //p2 turn
-    if (turns < 5) 
-    {
-      p2turn(p1, p2, board);
-
-      //check if game is over
-      if (gameover(board) == true)
-      {
-        return 0;
-      }
-
-      //draw board
-      print_board(board);
-    } 
-    else 
-    {
-      cout << "TIE!\n";
-      return 0;
-    }
-  }
-
-
-
-
+  
 }
