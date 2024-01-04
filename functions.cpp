1428ad024e8ee54b9b5d6cc8e7f05d8eb761465b
Joseph Attalla <jojoatt10@gmail.com>
2024-01-04 19:59:55 +0000

    Jan 4, 2024, 2:59 PM

--- functions.cpp
+++ functions.cpp
@@ -43,22 +43,22 @@ char players(const board[3][3])
   for (int i = 0; i < 3; i++) 
   {
     
-      for (int j = 0; j < 3; j++) 
-      {
-          if (board[i][j] != BOARD[i][j]) 
-          {
-              empty = false;
-              break;
-          }
-          if (board[i][j] == X)
-          {
-            X_count++;
-          }
-          else if (board[i][j] == O)
-          {
-            O_count++;
-          }
-      }
+    for (int j = 0; j < 3; j++) 
+    {
+        if (board[i][j] != BOARD[i][j]) 
+        {
+            empty = false;
+            break;
+        }
+        if (board[i][j] == X)
+        {
+          X_count++;
+        }
+        else if (board[i][j] == O)
+        {
+          O_count++;
+        }
+    }
   }
 
   if (empty)
