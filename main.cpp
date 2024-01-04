cd46dfe06bb0d534e0e14f8311a645681540c512
Joseph Attalla <jojoatt10@gmail.com>
2024-01-04 20:30:06 +0000

    Jan 4, 2024, 3:30 PM

--- main.cpp
+++ main.cpp
@@ -5,8 +5,28 @@
 using namespace std;
 
 
-int main() 
+int main()
 {
-  print_board(initial_state);
-  cout << player(initial_state) << endl;
+    // Example usage:
+    array<array<char, 3>, 3> exampleBoard = {
+        {'1', '2', '3'},
+        {'4', '5', '6'},
+        {'7', '8', '9'}
+    };
+
+    pair<int, int> exampleAction = make_pair(1, 1);
+
+    try
+    {
+        array<array<char, 3>, 3> newBoard = result(exampleBoard, exampleAction);
+
+        // Display the new board
+        print_board(newBoard.data());
+    }
+    catch (const runtime_error &e)
+    {
+        cerr << "Error: " << e.what() << endl;
+    }
+
+    return 0;
 }
