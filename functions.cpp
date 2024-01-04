b250cb56a89574e2ca161ce3f3552593b2533908
Joseph Attalla <jojoatt10@gmail.com>
2024-01-04 19:53:08 +0000

    Jan 4, 2024, 2:53 PM

--- functions.cpp
+++ functions.cpp
@@ -35,8 +35,8 @@ char players(const board[3][3])
   */
 
   // if board == BOARD, return X
-    // Compare each element of the board with the corresponding element in BOARD
-    for (int i = 0; i < 3; i++) {
+    for (int i = 0; i < 3; i++) 
+    {
         for (int j = 0; j < 3; j++) {
             if (board[i][j] != BOARD[i][j]) {
                 // The boards are different
